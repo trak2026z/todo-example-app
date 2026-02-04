7505f4f2375be998b5e3ede004f13d7b51ea988b
trak2026z <trak2026z@gmail.com>
2026-02-04 12:43:20 +0100

    feat: feature

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <div className="w-full max-w-md space-y-6">
         <h1 className="text-3xl font-bold text-center">Todo App</h1>
         nowa funkcjonalnosc2
+        nowa funkcjonalnosc3
         <TodoList />
       </div>
     </main>
